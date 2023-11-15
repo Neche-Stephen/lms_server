2381a23bc98101a3966c106a63df5277763130cb
Chinecherem <chinecheremubawike@gmail.com>
2023-11-15 10:28:05 +0100

    created a backend server for the lms

---
+++ getUserUID.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin')
+ async function UserUID(email){
+  return  admin.auth()
+  .getUserByEmail(email)
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    return userRecord.uid
+  })
+  .catch((error) => {
+    console.log('Error fetching user data:', error);
+  });
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+// import express from 'express';
+const express = require('express')
+const app = express();
+var admin = require("firebase-admin");
+// admin.auth().getu
+
+var serviceAccount = require("./rad5-lms.json");
+const  {UserUID } = require('./getUserUID');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+const assignrolebyuid = async (uid,role)=>{
+  try{
+    admin.auth()
+    .setCustomUserClaims(uid, { [role]: true })
+    .then((result) => {
+     
+    })
+    .catch((error) => {
+      throw new Error (error);
+    });
+  }catch(error){
+    throw new Error(error)
+  }
+}
+
+
+
+app.post('/setclaim', async (req, res)=>{
+  const {email,role} = req.body
+  const uid = await UserUID(email)
+try{
+   await assignrolebyuid(uid,role);
+    
+    res.status(200).json({message:'done'});
+} catch(err){
+      throw new Error(err)
+    }
+})
+
+
+app.listen(3000, ()=>{})
